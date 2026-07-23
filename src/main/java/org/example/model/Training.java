dcc4bed25b4dbb160dd804420dd608a90c9ea4e7
Buumty <wojtekand22@gmail.com>
2026-07-23 22:46:43 +0200

    Created Training class

--- src/main/java/org/example/model/Training.java
+++ src/main/java/org/example/model/Training.java
@@ -1,4 +1,47 @@
 package org.example.model;
 
+import java.time.Duration;
+import java.time.LocalDate;
+
 public class Training {
+    private final long traineeId;
+    private final long trainerId;
+    private final String trainingName;
+    private final TrainingType trainingType;
+    private final LocalDate trainingDate;
+    private final Duration trainingDuration;
+
+    public Training(long traineeId, long trainerId, String trainingName, TrainingType trainingType, LocalDate trainingDate, Duration trainingDuration) {
+        this.traineeId = traineeId;
+        this.trainerId = trainerId;
+        this.trainingName = trainingName;
+        this.trainingType = trainingType;
+        this.trainingDate = trainingDate;
+        this.trainingDuration = trainingDuration;
+    }
+
+    public long getTraineeId() {
+        return traineeId;
+    }
+
+    public long getTrainerId() {
+        return trainerId;
+    }
+
+    public String getTrainingName() {
+        return trainingName;
+    }
+
+
+    public TrainingType getTrainingType() {
+        return trainingType;
+    }
+
+    public LocalDate getTrainingDate() {
+        return trainingDate;
+    }
+
+    public Duration getTrainingDuration() {
+        return trainingDuration;
+    }
 }
