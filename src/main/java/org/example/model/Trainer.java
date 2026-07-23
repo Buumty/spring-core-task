a30ea8fedccadebf90800e2903b6b13dc37c2d75
Buumty <wojtekand22@gmail.com>
2026-07-23 22:33:06 +0200

    Created Trainer class

--- src/main/java/org/example/model/Trainer.java
+++ src/main/java/org/example/model/Trainer.java
@@ -1,4 +1,18 @@
 package org.example.model;
 
 public class Trainer extends User {
+    private TrainingType specialization;
+
+    public Trainer(long userId, String firstName, String lastName, String username, String password, boolean isActive, TrainingType specialization) {
+        super(userId, firstName, lastName, username, password, isActive);
+        this.specialization = specialization;
+    }
+
+    public TrainingType getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(TrainingType specialization) {
+        this.specialization = specialization;
+    }
 }
